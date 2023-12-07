ce88a854245256120c54bd845605be30b6fa4d07
Daniel <daniel353a3@gmail.com>
2023-12-07 14:09:36 +0200

    some changes in Jonatan code

--- scripts/home.js
+++ scripts/home.js
@@ -14,6 +14,7 @@ let posts = await fetchPosts();
 const postsDiv = document.getElementById("posts");
 console.log(posts);
 
+//create posts form api
 function showPosts(postsArr) {
     postsDiv.innerHTML = "";
 
@@ -24,19 +25,34 @@ function showPosts(postsArr) {
         const postDivTitle = document.createElement("div");
         const postBodyDiv = document.createElement("div");
         const postIdSpan = document.createElement("span");
+        const postImg = document.createElement("img");
 
+       // const skeletonContainer = document.createElement("div");
+        const skeletonTitle = document.createElement("div");
+        const skeletonBody = document.createElement("div");
+        
+
+        const postDivTitleParagraph = document.createElement("p");
+        
         postDiv.classList.add("post");
         postDivTitle.classList.add("post__title");
         postBodyDiv.classList.add("post__body");
         postIdSpan.classList.add("post-id");
+        postImg.classList.add("points");
+        
+        postDivTitleParagraph.classList.add("post__title--text");
 
-        postDivTitle.textContent = postObj.title;
+        postImg.src = "../assets/icons/points.png";
+        
+        postDivTitleParagraph.textContent = postObj.title;
         postBodyDiv.textContent = postObj.body;
         postIdSpan.textContent = `Post id: ${postObj.id}`;
 
+        postDivTitle.append(postDivTitleParagraph);
         postDiv.append(postDivTitle);
         postDiv.append(postBodyDiv);
         postDiv.append(postIdSpan);
+        postDiv.append(postImg);
 
         postsDiv.append(postDiv);
     }
